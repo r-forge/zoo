c1fe60ba9ed9e8d6cc229ddadfce69016b5f9a2c
azeileis (Achim Zeileis) <Achim.Zeileis@R-project.org>
2022-09-09 00:25:05 +0000

    The MATCH() methods for Date/POSIXct/POSIXlt object now coerce the "table" argument (if necessary) to Date or POSIXct, respectively, before calling match(unclass(x), unclass(table), ...) internally.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/zoo@1178 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/zoo/R/MATCH.R
+++ pkg/zoo/R/MATCH.R
@@ -13,17 +13,17 @@ MATCH.times <- function(x, table, nomatch = NA, units = "sec", eps = 1e-10, ...)
 }
 
 MATCH.Date <- function(x, table, nomatch = NA, ...) {
-  if(is.character(table)) table <- as.Date(table)
+  if(!inherits(table, "Date")) table <- as.Date(table)
   match(unclass(x), unclass(table), nomatch = nomatch, ...)
 }
 
 MATCH.POSIXct <- function(x, table, nomatch = NA, ...) {
-  if(is.character(table)) table <- as.POSIXct(table)
+  if(!inherits(table, "POSIXct")) table <- as.POSIXct(table)
   match(unclass(x), unclass(table), nomatch = nomatch, ...)
 }
 
 MATCH.POSIXlt <- function(x, table, nomatch = NA, ...) {
   x <- as.POSIXct(x)
-  if(inherits(table, "POSIXlt") || is.character(table)) table <- as.POSIXct(table)
+  if(!inherits(table, "POSIXct")) table <- as.POSIXct(table)
   match(unclass(x), unclass(table), nomatch = nomatch, ...)
 }
